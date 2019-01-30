80c21e98a40f0ba5f60de22a40f2f03cb72778ad
Max Atkinson <max@Maxs-MacBook-Pro-4.local>
2019-01-30 21:48:23 +0000

    css and form

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,26 @@
 const socket = io.connect('http://localhost:3000');
 const room = 'defaultRoom';
 
-socket.on('connect', connectHandler);
-socket.on('message', handleReceiveMessage);
+socket.on('connect', handleConnect);
+socket.on('message', handleMessage);
 socket.on(`${room}Joined`, console.log);
 
-function connectHandler() {
+function handleConnect() {
     console.log('Connected to socket.io server.');
 }
 
-function handleSendMessage() {
-    const message = $('#input').val();
+function handleMessage(msg) {
+    const $para = $('<p></p>').text(msg);
+    $('#chat').append($para);
+}
+
+function handleSendMessage(e) {
+    e.preventDefault();
+    const $input = $('#input');
+    const message = $input.val();
     if (message.trim() !== '') {
         console.log(message);
         socket.emit('message', message);
+        $input.val('');
     }
-}
-
-function handleReceiveMessage(msg) {
-    const $para = $('<p></p>').text(msg);
-    $('#chat').append($para);
 }
