b03962c20d776bfd74850d9839438b355e07a18c
Max Atkinson <max@Maxs-MacBook-Pro-4.local>
2019-01-31 14:47:56 +0000

    configureSockets pulled into fn

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import socketio from 'socket.io';
-import { getChannel } from './sockets';
+import configureSockets from './sockets';
 
 // Init web app
 const app = express();
 const server = http.Server(app)
 const io = socketio.listen(server);
+configureSockets(io);
 
 // Set app props
 app.set('port', 3000);
@@ -21,27 +22,6 @@ app.use(express.static(path.join(__dirname, '../public')));
 // Serve the socket.io client from node_modules
 app.get('/socket.io-client.js', (req, res) => {
     res.sendFile(path.join(__dirname, '../node_modules/socket.io-client/dist/socket.io.min.js'));
-});
-
-// Set up socket.io handlers
-io.on('connection', function (socket) {
-    // https://socket.io/docs/emit-cheatsheet/
-
-    socket.join('defaultRoom');
-    io.to('defaultRoom').emit('defaultRoomJoined', 'someone joined the room');
-    io.to('defaultRoom').emit('message', 'welcome!');
-    socket.emit('test', { test: 'only you can see this' });
-
-    socket.on('message', (msg) => {
-        io.to(getChannel(socket)).emit('message', msg);
-    });
-
-    socket.on('channelChange', (channel) => {
-        socket.leaveAll();
-        socket.join(channel, () => {
-            console.log(getChannel(socket));
-        });
-    });
 });
 
 // Listen on our port
--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,3 +1,26 @@
 export function getChannel(socket) {
     return Object.keys(socket.rooms).find(r => r);
 }
+
+export default function configureSockets(io) {
+    // Set up socket.io handlers
+    io.on('connection', function (socket) {
+        // https://socket.io/docs/emit-cheatsheet/
+
+        socket.join('defaultRoom');
+        io.to('defaultRoom').emit('defaultRoomJoined', 'someone joined the room');
+        io.to('defaultRoom').emit('message', 'welcome!');
+        socket.emit('test', { test: 'only you can see this' });
+
+        socket.on('message', (msg) => {
+            io.to(getChannel(socket)).emit('message', msg);
+        });
+
+        socket.on('channelChange', (channel) => {
+            socket.leaveAll();
+            socket.join(channel, () => {
+                console.log(getChannel(socket));
+            });
+        });
+    });
+}
