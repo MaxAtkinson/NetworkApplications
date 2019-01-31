361bed4cda5bc9f7d718330c28db842797ff23b2
Max Atkinson <max@Maxs-MacBook-Pro-4.local>
2019-01-31 16:50:42 +0000

    join default room

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -3,20 +3,23 @@ export default function configureSockets(io) {
     io.on('connection', function (socket) {
         // https://socket.io/docs/emit-cheatsheet/
 
-        socket.join('defaultRoom');
-        io.to('defaultRoom').emit('defaultRoomJoined', 'someone joined the room');
-        io.to('defaultRoom').emit('message', 'welcome!');
-        socket.emit('test', { test: 'only you can see this' });
-
-        socket.on('message', (msg) => {
-            io.to(getChannel(socket)).emit('message', msg);
-        });
-
-        socket.on('channelChange', (channel) => {
-            socket.leaveAll();
-            socket.join(channel, () => {
+        socket.join('defaultRoom', () => {
+            io.to('defaultRoom').emit('defaultRoomJoined', 'someone joined the room');
+            io.to('defaultRoom').emit('message', 'welcome!');
+            socket.emit('test', { test: 'only you can see this' });
+    
+            socket.on('message', (msg) => {
+                io.to(getChannel(socket)).emit('message', msg);
                 console.log(getChannel(socket));
-                socket.emit('channelChanged', channel);
+                console.log(msg);
+            });
+    
+            socket.on('channelChange', (channel) => {
+                socket.leaveAll();
+                socket.join(channel, () => {
+                    console.log(getChannel(socket));
+                    socket.emit('channelChanged', channel);
+                });
             });
         });
     });
