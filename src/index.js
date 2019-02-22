29f20fd301ba4a5b1ccbdc0951ab8c6e417b491f
anowlan123 <aln2@hw.ac.uk>
2019-02-22 13:01:54 +0000

    uncommented

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
 configureSockets(io);
-// configureAuth(app, jwt, dbUrl);
+configureAuth(app, jwt, dbUrl);
 
 // Serve the socket.io client from node_modules
 app.get('/socket.io-client.js', (req, res) => {
